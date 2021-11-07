97289d057205c45d2e234dbdccfe6304b071616b
matteocelani <matteocelani4@gmail.com>
2021-11-07 18:28:21 +0100

    Section 3
    ⬇️ Getting the code.
    🔌 Building a connect to wallet button.
    🚏 Setting up the initial app state.
    🔎 Checking for a Character NFT.
    🏹 Building the Character Select page.
    ⚔️ Building the Arena page.

---
+++ main/src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = '0xC3f55581A90d971f914dE2D9eFbB386baF1df1Dd';
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformCharacterData };
